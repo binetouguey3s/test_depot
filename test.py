053f00e3dbd691f47427b3a9e9dde8095c6a76d7
binetouguey3s <binetouguey3s@gmail.com>
2026-01-14 15:09:52 +0000

    Ajout du fichier test.py

---
+++ test.py
@@ -0,0 +1,17 @@
+Liste_contact = []
+
+# Contraintes :
+# utiliser un dictionnaire pour représenter un contact,
+contact = {
+    "nom": "Gueye",
+    "prenom":"Astou",
+    "numero":"771234567",
+    "adresse_e_mail":"gueyastou@gmail.com"
+}
+
+contact_1= ["Gueye","Astou","771234567","gueyastou@gmail.com"]
+
+# les ajouter à la liste,
+Liste_contact.extend(contact_1)
+print(Liste_contact)
+
